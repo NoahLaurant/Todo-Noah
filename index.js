754f14ff2dd6143d20f9aad3faed1caa9119fc5f
NoahLaurant <101389192+NoahLaurant@users.noreply.github.com>
2026-05-28 15:43:18 +0200

    maj

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express"
+import dotenv from "dotenv"
+import { createClient } from "@supabase/supabase-js"
 const app = express()
 const port = 3000
+
+//lit le fichier .env
+dotenv.config()
 
 //on utilise le moteur de template EJS
 app.set("view engine","ejs")
@@ -8,25 +13,34 @@ app.set("view engine","ejs")
 //mes assets et mon style sont dans le dossier public
 app.use(express.static('public'))
 
+//permet de lire correctement le contenu des données envoyées par le formulaire
+app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
-    const user = {
-        nom:"Laurant",
-        prenom:"Noah"
-    }
+
+//Base de donnée:
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+
+app.get('/', async(req, res) => {
+  const { data, error } = await supabase
+    .from('Todo')
+    .select('*')
+ 
+   console.log(data)
   res.render("home",{"user": user})
 })
 
 //route le formulaire pour creer une tache
 app.get("/ajouter",(req,rep)=>{
+
     rep.render("ajouter.ejs")
 })
 //route qui reçoit les donnee du formulaire
-app.post("/ajouter",(req,res)=>{
-    console.log(req)
+app.post("/ajouter", async(req,res)=>{
+   // console.log(req.body)
+   await supabase.from('todos').insert(req.body)
 })
 
-app.use(express.urlencoded({extended: true}))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
